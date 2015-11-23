7165b3aecbf6665c11130c82d11f0ce0d1b8be39
Rich Megginson <rmeggins@redhat.com>
2015-11-23 16:16:13 -0700

    moved nulecule stuff to nulecule-rsyslog

--- build-image.sh
+++ build-image.sh
@@ -4,9 +4,7 @@ set -ex
 prefix=${PREFIX:-${1:-bitscout/}}
 version=${VERSION:-${2:-latest}}
 docker build -t "${prefix}rsyslog:${version}" .
-docker build -t "${prefix}rsyslog-app:${version}" nulecule/
 
 if [ -n "${PUSH:-$3}" ]; then
 	docker push "${prefix}rsyslog:${version}"
-	docker push "${prefix}rsyslog-app:${version}"
 fi
