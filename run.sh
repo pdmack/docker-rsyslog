dc8a93b54e3fbf903a030dabff9f86b8604ec377
Anton Sherkhonov <sherkhonov@gmail.com>
2016-05-19 18:59:04 +0000

    fixed rsyslog config following discussion w/ peter

--- run.sh
+++ run.sh
@@ -7,6 +7,9 @@ ES_HOST=${ES_HOST:-viaq-elasticsearch}
 ES_PORT=${ES_PORT:-9200}
 SYSLOG_LISTEN_PORT=${SYSLOG_LISTEN_PORT:-10514}
 DEBUG_RSYSLOG=${DEBUG_RSYSLOG:-true}
+LOGSTASH_PREFIX=${LOGSTASH_PREFIX:-logstash-}
+NORMALIZER_NAME=${NORMALIZER_NAME:-container-rsyslog8.17}
+NORMALIZER_IP=${NORMALIZER_IP:-172.17.77.14}
 
 if [ "$DEBUG_RSYSLOG" = true ]; then
     RSYSLOG_ARGS="-d -n"
@@ -18,9 +21,7 @@ if [ -f "/data/rsyslog.conf" ]; then
     cp /data/rsyslog.conf /etc/rsyslog.conf
     rm /etc/rsyslog.d/*.conf
     if [ -d "/data/rsyslog.d" ]; then
-        for config in /data/rsyslog.d/*.conf; do
-            cp "$config" /etc/rsyslog.d/
-        done
+        cp -R /data/rsyslog.d /etc/
     fi
 fi
 
@@ -28,6 +29,9 @@ for file in /etc/rsyslog.conf /etc/rsyslog.d/*.conf ; do
     if [ ! -f "$file" ] ; then continue ; fi
     sed -i -e "s/%ES_HOST%/$ES_HOST/g" -e "s/%ES_PORT%/$ES_PORT/g" \
         -e "s/%SYSLOG_LISTEN_PORT%/$SYSLOG_LISTEN_PORT/g" \
+        -e "s/%LOGSTASH_PREFIX%/$LOGSTASH_PREFIX/g" \
+        -e "s/%NORMALIZER_NAME%/$NORMALIZER_NAME/g" -e "s/%NORMALIZER_IP%/$NORMALIZER_IP/g" \
         "$file"
 done
 /usr/sbin/rsyslogd ${RSYSLOG_ARGS}
+
