a9b062756bb5e4baca7e0f9f8172935b7b95412a
Anton Sherkhonov <sherkhonov@gmail.com>
2018-06-01 13:38:42 +0000

    Added support for KUBECONFIG

--- openshift/deploy-normalizer.sh
+++ openshift/deploy-normalizer.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-NAMESPACE=${NAMESPACE:-dh-stage-ingest}
+set -o errexit
+set -o nounset
+set -o pipefail
 
-oc create configmap rsyslog-d-conf -n ${NAMESPACE} --from-file ../rsyslog.d --dry-run -o yaml > /tmp/rsyslog-d-conf.yaml
-oc apply -f /tmp/rsyslog-d-conf.yaml
+export NAMESPACE=${NAMESPACE:-dh-stage-ingest}
 
-oc apply -f rsyslog-deployment.yaml -n ${NAMESPACE}
+if [ ${KUBECONFIG:-""} = "" ]; then
+    export USE_KUBECONFIG=""
+else
+	export USE_KUBECONFIG="--config ${KUBECONFIG}"
+fi
 
-oc apply -f rsyslog-service.yaml -n ${NAMESPACE}
+oc create configmap rsyslog-d-conf -n ${NAMESPACE} ${USE_KUBECONFIG} --from-file ../rsyslog.d --dry-run -o yaml > /tmp/rsyslog-d-conf.yaml
+oc apply -f /tmp/rsyslog-d-conf.yaml ${USE_KUBECONFIG}
+
+oc apply -f rsyslog-deployment.yaml -n ${NAMESPACE} ${USE_KUBECONFIG}
+
+oc apply -f rsyslog-service.yaml -n ${NAMESPACE} ${USE_KUBECONFIG}
